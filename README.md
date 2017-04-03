56fbfa50a95121750bd5c5391771d3b59c2ed873
r-lyeh <rlyeh.2015@gmail.com>
2017-04-03 17:59:55 +0200

    Update to latest CLDR data

--- README.md
+++ README.md
@@ -6,16 +6,16 @@ For anyone interested, I just dumped most of the CLDR data in a compact way (see
 The final data for all languages is 13,727,497 bytes, but still highly compressible, as seen below.
 
 ```lisp
-BSC:    1,023,137 bytes, ratio=92.5468% enctime=1211822us dectime=757387us 
-BROTLI: 1,287,148 bytes, ratio=90.6236% enctime=67236011us dectime=50243us 
-LZMA25: 1,369,212 bytes, ratio=90.0258% enctime=3961970us dectime=98609us 
-LZIP:   1,369,811 bytes, ratio=90.0214% enctime=3895218us dectime=131528us 
-LZMA20: 1,423,667 bytes, ratio=89.6291% enctime=3334007us dectime=103905us 
-MINIZ:  1,892,977 bytes, ratio=86.2103% enctime=697077us dectime=31830us 
-ZSTD:   2,108,053 bytes, ratio=84.6436% enctime=65694us dectime=34525us 
-LZ4HC:  2,151,652 bytes, ratio=84.326% enctime=491871us dectime=13641us 
-LZ4:    2,918,991 bytes, ratio=78.7362% enctime=37851us dectime=13775us 
-RAW:   13,727,497 bytes, ratio=0% enctime=16242us dectime=7658us 
+BSC:    1,023,137 bytes, ratio=92.5468% enctime=1211822us dectime=757387us
+BROTLI: 1,287,148 bytes, ratio=90.6236% enctime=67236011us dectime=50243us
+LZMA25: 1,369,212 bytes, ratio=90.0258% enctime=3961970us dectime=98609us
+LZIP:   1,369,811 bytes, ratio=90.0214% enctime=3895218us dectime=131528us
+LZMA20: 1,423,667 bytes, ratio=89.6291% enctime=3334007us dectime=103905us
+MINIZ:  1,892,977 bytes, ratio=86.2103% enctime=697077us dectime=31830us
+ZSTD:   2,108,053 bytes, ratio=84.6436% enctime=65694us dectime=34525us
+LZ4HC:  2,151,652 bytes, ratio=84.326% enctime=491871us dectime=13641us
+LZ4:    2,918,991 bytes, ratio=78.7362% enctime=37851us dectime=13775us
+RAW:   13,727,497 bytes, ratio=0% enctime=16242us dectime=7658us
 ```
 
 This is what is currently processed from the CLDR repos:
@@ -79,4 +79,4 @@ cldr-segments-modern\segments\xx-XX
 
 ## Licenses
 - [CLDR](https://github.com/r-lyeh/cldr), public domain.
-- http://unicode.org/copyright.html, original license for the data. 
+- http://unicode.org/copyright.html, original license for the data.
--- cldr.cc
+++ cldr.cc
@@ -9,16 +9,16 @@ For anyone interested, I just dumped most of the CLDR data in a compact way (see
 The final data for all languages is 13,727,497 bytes, but still highly compressible, as seen below.
 
 ```lisp
-BSC:    1,023,137 bytes, ratio=92.5468% enctime=1211822us dectime=757387us 
-BROTLI: 1,287,148 bytes, ratio=90.6236% enctime=67236011us dectime=50243us 
-LZMA25: 1,369,212 bytes, ratio=90.0258% enctime=3961970us dectime=98609us 
-LZIP:   1,369,811 bytes, ratio=90.0214% enctime=3895218us dectime=131528us 
-LZMA20: 1,423,667 bytes, ratio=89.6291% enctime=3334007us dectime=103905us 
-MINIZ:  1,892,977 bytes, ratio=86.2103% enctime=697077us dectime=31830us 
-ZSTD:   2,108,053 bytes, ratio=84.6436% enctime=65694us dectime=34525us 
-LZ4HC:  2,151,652 bytes, ratio=84.326% enctime=491871us dectime=13641us 
-LZ4:    2,918,991 bytes, ratio=78.7362% enctime=37851us dectime=13775us 
-RAW:   13,727,497 bytes, ratio=0% enctime=16242us dectime=7658us 
+BSC:    1,023,137 bytes, ratio=92.5468% enctime=1211822us dectime=757387us
+BROTLI: 1,287,148 bytes, ratio=90.6236% enctime=67236011us dectime=50243us
+LZMA25: 1,369,212 bytes, ratio=90.0258% enctime=3961970us dectime=98609us
+LZIP:   1,369,811 bytes, ratio=90.0214% enctime=3895218us dectime=131528us
+LZMA20: 1,423,667 bytes, ratio=89.6291% enctime=3334007us dectime=103905us
+MINIZ:  1,892,977 bytes, ratio=86.2103% enctime=697077us dectime=31830us
+ZSTD:   2,108,053 bytes, ratio=84.6436% enctime=65694us dectime=34525us
+LZ4HC:  2,151,652 bytes, ratio=84.326% enctime=491871us dectime=13641us
+LZ4:    2,918,991 bytes, ratio=78.7362% enctime=37851us dectime=13775us
+RAW:   13,727,497 bytes, ratio=0% enctime=16242us dectime=7658us
 ```
 
 This is what is currently processed from the CLDR repos:
@@ -82,6 +82,7 @@ cldr-segments-modern\segments\xx-XX
 
 */
 
+#include <functional>
 #include <fstream>
 #include <sstream>
 #include <iostream>
@@ -334,7 +335,7 @@ int main( int argc, const char **argv ) {
 
     std::cout << "Reading disk structure..." << std::endl;
 
-    for( auto &file : apathy::lsr("*.json") ) {
+    for( auto &file : apathy::ls("**.json") ) {
         std::string basename = file.substr( file.find_last_of('/') + 1 );
         std::cout << "[ ] " << file << "...";
         if( auto parser = parsers[basename] ) {
